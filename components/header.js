8eecbcb6109b117c97bb79fbcd99ff39ee1b2149
jamesondh <jamesondh@gmail.com>
2020-08-31 17:01:32 -0400

    Add bnc-onboard for connecting to wallet (WIP)

--- components/header.js
+++ components/header.js
@@ -1,10 +1,59 @@
+import React, { useState, useEffect } from 'react';
+
 // UI elements
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+// import ProfileHover from 'profile-hover';
+
+import Onboard from 'bnc-onboard';
+import Web3 from 'web3';
+import { apiKeys } from "../api-keys"
+// import dynamic from 'next/dynamic'
+
+// Store instantiated web3
+let web3;
+
+// Initialize Onboard
+// const Onboard = dynamic(
+//   () => import('bnc-onboard'),
+//   { ssr: false }
+// )
+
 
 export default function Header() {
+  
+  const [onboard, setOnboard] = useState(null)
+  
+  useEffect(() => {
+//     const onboard = ;
+    setOnboard(Onboard({
+      dappId: apiKeys.blocknativeKey,
+      networkId: 3,
+      subscriptions: {
+        wallet: wallet => {
+          // Instantiate web3 when the user has selected a wallet
+          web3 = new Web3(wallet.provider);
+          console.log(`${wallet.name} connected!`);
+        }
+      }
+    }))
+  });
+  
+  let [addr, updateAddr] = useState(null);
+
+  async function walletLogin() {
+
+    // Connect to Onboard
+    await onboard.walletSelect();
+//     await onboard.walletCheck();
+
+//     // Get account
+//     const accounts = await web3.eth.getAccounts();
+//     const address = accounts[0];
+//     updateAddr(address);
+  }  
   return (
     <header data-testid="header">
       <Navbar expand="lg" className="my-3">
@@ -20,7 +69,7 @@ export default function Header() {
           </Nav>
           <Nav>
             <Nav.Link>
-              <Button variant="primary" onClick={console.log("Connect to wallet")}>Connect to wallet</Button><br/>
+              <Button variant="primary" onClick={walletLogin}>Connect to wallet</Button><br/>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
